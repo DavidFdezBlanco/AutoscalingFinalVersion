0133899857fe608ad4b357cee830c47428fbfc1d
Fernandezblanco <Fernandezblanco@LY-FERNANDEZB>
2019-07-22 14:25:56 +0200

    Add project files.

---
+++ AutoscalingFinalVersion/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AutoscalingFinalVersion
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //Service status log report
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 500000; //number in milisecinds  
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+    }
+}
