c77f7b5e0df6d6bca11f0055a9c871d07bf957af
Fernandezblanco <Fernandezblanco@LY-FERNANDEZB>
2019-07-22 15:05:39 +0200

    FirstCommit

--- AutoscalingFinalVersion/Service1.Designer.cs
+++ AutoscalingFinalVersion/Service1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AutoscalingFinalVersion
+﻿namespace MyFirstService
 {
     partial class Service1
     {
--- AutoscalingFinalVersion/Service1.cs
+++ AutoscalingFinalVersion/Service1.cs
@@ -3,35 +3,63 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
-
-namespace AutoscalingFinalVersion
+namespace MyFirstService
 {
     public partial class Service1 : ServiceBase
     {
-        Timer timer = new Timer();
-
+        Timer timer = new Timer(); // name space(using System.Timers;)  
         public Service1()
         {
             InitializeComponent();
         }
-
         protected override void OnStart(string[] args)
         {
-            //Service status log report
-            WriteToFile("Service is started at " + DateTime.Now);
+            WriteToLogFile("Service is started at " + DateTime.Now);
             timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-            timer.Interval = 500000; //number in milisecinds  
+            timer.Interval = 50000; //number in milisecinds  
             timer.Enabled = true;
         }
-
         protected override void OnStop()
         {
-            WriteToFile("Service is stopped at " + DateTime.Now);
+            WriteToLogFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToLogFile("Service is recall at " + DateTime.Now);
+        }
+
+
+
+
+        public void WriteToLogFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
         }
     }
 }
